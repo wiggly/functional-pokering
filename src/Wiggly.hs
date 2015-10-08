42a4c5ddf83d354eeb8af6aabe5b85ab4a45e3b8
Nigel Rantor <wiggly@wiggly.org>
2015-10-08 11:23:26 +0100

    Removed incorrect combinations function
    Added comboCount to determine number of potential n choose k combos.

--- src/Wiggly.hs
+++ src/Wiggly.hs
@@ -3,28 +3,30 @@
 module Wiggly
        (
          nChooseK,
-         chunkList
+         chunkList,
+         comboCount
        )
        where
+
+comboCount :: Int -> Int -> Int
+comboCount n k
+  | n < k = error "Cannot choose more than the number of elements"
+  | n == k = 1
+  | k == 1 = n
+  | otherwise = (comboCount (n-1) k) + (comboCount (n-1) (k-1))
 
 -- n choose k
 nChooseK :: [a] -> Int -> [[a]]
 nChooseK xs 1 = map (\x -> [x]) xs
 nChooseK xs n
-  | (length xs) > n = foo ++ bar 
-  | (length xs) == n = [xs] 
+  | (length xs) > n = foo ++ bar
+  | (length xs) == n = [xs]
   | otherwise = []
   where foo = map (\x -> (head xs):x ) (nChooseK (tail xs) (n-1))
         bar = (nChooseK (tail xs) n)
 
-combinations :: Int -> Int -> Int
-combinations n k
-  | n == k = 1
-  | otherwise = combinations (n-1) (k-1)  
-
-chunkList :: Int -> [a] -> [[a]]  
+chunkList :: Int -> [a] -> [[a]]
 chunkList _ [] = []
 chunkList n xs = thisChunk : (chunkList n thisRemainder)
   where thisChunk = take n xs
         thisRemainder = drop n xs
-
